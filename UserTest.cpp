5f96b426d69f3c442b784dba2874ff3ea3f4fd73
cagedfaraday <cagedfaraday@yaho.com>
2023-06-10 20:11:24 -0700

    moved tests to UserTest

---
+++ UserTest.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include "User.h"
+
+using namespace std;
+
+
+int testLogin() {
+  int testsPassed{};
+
+  User user1;
+  user1.username("user1");
+  user1.password("PAssWOrd#$12");
+
+  User user2;
+  user2.username("user2");
+  user2.password("PAssWOrd12#$");
+
+
+  cout << "############TEST#############\n";
+  //Passes test if login fails
+  cout << "user1 -- login: user1, password: PassWOrd#$12\n";
+  if (!user1.login("", "")) {
+    cout << "login: , password: \n";
+    cout << "testLogin 1 passed\n";
+    testsPassed++;
+  }
+
+  //Passes test if login fails
+  if(!user1.login("user1", "PAssWOrd12#$")) {
+    cout << "login: user1, password: PAssWOrd12#$\n";
+    cout << "testLogin 2 passed \n";
+    testsPassed++;
+  }
+  //Passes test if login suceeds
+  if (user1.login("user1", "PAssWOrd#$12")) {
+    cout << "login: user1, password: PassWOrd#$12\n";
+    cout << "testLogin 3 passed \n";
+    testsPassed++;
+  }
+
+  cout << "############TEST#############\n";
+  cout << "user2 -- login: user2 , password:PAssWOrd12#$\n";
+  if (!user2.login("", "")) {
+    cout << "login: , password: \n";
+    cout << "testLogin 1 passed\n";
+    testsPassed++;
+  }
+  if(user2.login("user2", "PAssWOrd12#$")) {
+    cout << "login: user2, password: PAssWOrd12#$\n";
+    cout << "testLogin 2 passed \n";
+    testsPassed++;
+  }
+  if (!user2.login("user2", "PAssWOrd#$12")) {
+    cout << "login: user2, password: PAssWOrd#$12\n";
+    cout << "testLogin 3 passed \n";
+    testsPassed++;
+  }
+
+  cout << "#############################\n";
+  return testsPassed;
+}
+
+int runUserTests() {
+  int numPassed{};
+
+  numPassed += testLogin();
+  return numPassed;
+}
--- main.cpp
+++ main.cpp
@@ -1,17 +1,8 @@
 #include "User.h"
+#include "UserTest.h"
 #include <iostream>
 
 using namespace std;
-
-User users[2];
-
-bool login(string username, string password) {
-  int numUsers{size(users)};
-  for (int i{0}; i < numUsers; i++) {
-	  if (users[i].login(username, password)) return true;	
-  }
-  return false;
-}
 
 /*void handleLogin() {
   bool sessionExists {false};
@@ -29,44 +20,11 @@ bool login(string username, string password) {
   }  
 }*/
 
-int testLogin() {
-  int testsPassed{0};
-
-  cout << "############TEST#############\n";
-  if (!login("", "")) {
-    //cout << "login: , password: \n";
-    cout << "testLogin 1 passed\n";
-    testsPassed++;
-  }
-  if(!login("user1", "PAssWOrd12#$")) {
-    //cout << "login: user1, password: PAssWOrd12#$\n";
-    cout << "testLogin 2 passed \n";
-    testsPassed++;
-  }
-  if (login("user1", "PAssWOrd#$12")) {
-    //cout << "login: User1, password: PAssWOrd#$12\n";
-    cout << "testLogin 3 passed \n";
-    testsPassed++;
-  }
-
-  cout << "#############################\n";
-  return testsPassed;
-}
-
 int main () {
-  User user1;
-  user1.username("user1");
-  user1.password("PAssWOrd#$12");
-  
-  User user2;
-  user2.username("user2");
-  user2.password("PAssWOrd12#$");
-  
-  users[0] = user1;
-  users[1] = user2;
-
   // bool sessionExists {false};
-  int testsPassed = testLogin();
+  int testsPassed{};
+
+  testsPassed += runUserTests();
   cout << "Tests Passed: " << testsPassed <<" \n";
   return 0;
 }
